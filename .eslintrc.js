650ebc54c6a3f85221c2aa595499fa648de15e10
Raghav_Dhir <raghav.dhir@leadsquared.com>
2023-05-17 20:37:54 +0530

    initialise project

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+
+    // TypeScript-specific rules
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // React-specific rules
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+
+    // Spacing rules
+    indent: ['error', 2],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2]
+
+  }
+}
